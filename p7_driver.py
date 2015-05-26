f459f4c007dabc8807729502f32c9317c855f15b
Ricardo-Murillo <aydonspek@gmail.com>
2015-05-25 21:15:04 -0700

    Update p7_driver.py

--- p7_driver.py
+++ p7_driver.py
@@ -6,19 +6,23 @@ import sys
 import p7_visualize
 
 def solve(*args):
+  GRINGO = "./gringo"
+  REIFY = "./reify"
   CLINGO = "./clingo"
-  clingo = subprocess.Popen(
-    [CLINGO, "--outf=2"] + list(args),
+  gringo = subprocess.Popen(
+    [GRINGO, "level-core.lp","level-style.lp","level-sim.lp","level-shortcuts.lp"],
+    stdout = subprocess.PIPE,
+    stderr = subprocess.PIPE)
+  reify = subprocess.Popen([REIFY],
+    stdin = gringo.stdout,
+    stdout = subprocess.PIPE,
+    stderr = subprocess.PIPE)
+  clingo = subprocess.Popen([CLINGO, "-","meta.lp","metaD.lp","metaO.lp","metaS.lp","-c","width=10","--parallel-mode=4","--outf=2", "--sign-def=3","--seed="+str(random.randint(0,1<<30))],
+    stdin = reify.stdout,
     stdout = subprocess.PIPE,
     stderr = subprocess.PIPE)
   out, err = clingo.communicate()
   if err:
     print err
   return p7_visualize.loadAndBuild(out)
- 
-def solve_randomly(*args):
-    """Like solve() but uses a random sign heuristic with a random seed."""
-    args = list(args) + ["--sign-def=3","--seed="+str(random.randint(0,1<<30))]
-    return solve(*args) 
-    
-solve_randomly("level-core.lp","-c","width=10")
+solve()
